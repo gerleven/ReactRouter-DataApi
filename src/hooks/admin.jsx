96390195970e39af2529702c4ad80d07bea6d99a
German Levental <gerleven@gmail.com>
2023-10-10 13:40:50 -0300

    update

--- src/hooks/admin.jsx
+++ src/hooks/admin.jsx
@@ -42,26 +42,33 @@ export const useSessionTime = () => {
 
 export const useExampleContact = ()=>{
   const [firstTime, setFirstTime] = useState(true)
-  if(firstTime===true){
-    getContacts().then(
-      (contactList)=>{
-        setFirstTime(false);
-        if(contactList.length===0){
-          let contact = { 
-            "id": "exampleId",
-            "createdAt": 1696941836452,
-            "first": "German",
-            "last": "Levental",
-            "twitter": "@germanlevental",
-            "avatar": "https://media.licdn.com/dms/image/C4E03AQEYqNIBWkR25g/profile-displayphoto-shrink_200_200/0/1556768978154?e=1701302400&v=beta&t=LM8LtMFOf7XEpWBreLzg9M-zoQMmZ8OWQYNmS2caT24",
-            "notes": "This is an example of contact!",
-            "favorite": true
-          };
-          createContact(contact).then(()=>{
-            useSubmit(null, {action: "/"});
-          });
+  const submit=useSubmit();
+
+  useEffect(()=>{
+    const id = Math.random().toString(36).substring(2, 9);
+    const createdAt= Date.now();
+    
+    if(firstTime===true){
+      getContacts().then(
+        (contactList)=>{
+          setFirstTime(false);
+          if(contactList.length===0){
+            let contact = { 
+              "id": id,
+              "createdAt": createdAt,
+              "first": "German",
+              "last": "Levental",
+              "twitter": "@germanlevental",
+              "avatar": "https://media.licdn.com/dms/image/C4E03AQEYqNIBWkR25g/profile-displayphoto-shrink_200_200/0/1556768978154?e=1701302400&v=beta&t=LM8LtMFOf7XEpWBreLzg9M-zoQMmZ8OWQYNmS2caT24",
+              "notes": "This is an example of contact!",
+              "favorite": true
+            };
+            createContact(contact).then(()=>{
+              submit(null, {action: "/"});
+            });
+          }
         }
-      }
-    )
-  }
+      )
+    }
+  },[]);
 }
